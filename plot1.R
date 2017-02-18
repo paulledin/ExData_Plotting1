707f95a709af9c7579914871e289112ac912ffd4
Paul Ledin <paul_ledin@uwalumni.com>
2017-02-18 15:48:02 -0600

    Exploratory Data Analysis Plot 1

---
+++ plot1.R
@@ -0,0 +1,18 @@
+#Reproduce Plot 1
+
+powerconsumption <- read.table(".\\data\\household_power_consumption.txt", header=TRUE, 
+                               sep=";", stringsAsFactors=FALSE, na.strings="?")
+
+powerconsumption$Timestamp = strptime(paste(powerconsumption$Date, 
+                                            powerconsumption$Time), 
+                                      format = "%d/%m/%Y %H:%M:%S")
+
+powerconsumption$Date <- as.Date(powerconsumption$Date, format="%d/%m/%Y")
+powerconsumption <- powerconsumption[(powerconsumption$Date=="2007-02-01" |
+                                          powerconsumption$Date=="2007-02-02"),]
+
+png(file="plot1.png", width = 480, height = 480)
+hist(powerconsumption$Global_active_power, main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)",
+     col="Red")
+dev.off()
